433f7a08c4e78846bdd4b644e8267c86f49d8e0f
Blez_Ten <83898491+blezDev@users.noreply.github.com>
2023-03-14 21:45:00 +0530

    added repo class

--- app/src/main/java/com/blez/compilerx/repository/CompilerRepository.kt
+++ app/src/main/java/com/blez/compilerx/repository/CompilerRepository.kt
@@ -1,8 +1,12 @@
 package com.blez.compilerx.repository
 
 import com.blez.compilerx.data.api.CompilerAPI
+import com.blez.compilerx.data.model.CodeBody
 
 class CompilerRepository(val api: CompilerAPI) {
+    suspend fun cCompile(code : CodeBody) = api.cCompile(code)
+    suspend fun javaCompile(code: CodeBody) = api.javaCompile(code)
 
+    suspend fun python(code: CodeBody) = api.pythonCompile(code)
 
 }
--- app/src/main/java/com/blez/compilerx/ui/compiler/CompilerViewModel.kt
+++ app/src/main/java/com/blez/compilerx/ui/compiler/CompilerViewModel.kt
@@ -1,9 +1,26 @@
 package com.blez.compilerx.ui.compiler
 
 import androidx.lifecycle.ViewModel
+import com.blez.compilerx.repository.CompilerRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
 
 @HiltViewModel
-class CompilerViewModel : ViewModel() {
+class CompilerViewModel @Inject constructor(repo : CompilerRepository) : ViewModel() {
+
+    sealed class SetupEvent{
+        object nothing : SetupEvent()
+
+        object Loading : SetupEvent()
+
+
+
+    }
+
+    private val _uiState = MutableStateFlow<SetupEvent>(SetupEvent.nothing)
+    val uiState : StateFlow<SetupEvent>
+    get() = _uiState
 
 }
