5fc1e0962d2c53c1606e23c89a34ad3ee754b765
Blez_Ten <83898491+blezDev@users.noreply.github.com>
2023-03-14 21:47:29 +0530

    added repo class

--- app/src/main/java/com/blez/compilerx/di/AppModule.kt
+++ app/src/main/java/com/blez/compilerx/di/AppModule.kt
@@ -1,12 +1,21 @@
 package com.blez.compilerx.di
 
+import com.blez.compilerx.data.api.CompilerAPI
+import com.blez.compilerx.repository.CompilerRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun providesCompilerRepository(compilerAPI : CompilerAPI) : CompilerRepository{
+        return CompilerRepository(compilerAPI)
+    }
 
 }
