3b395343727c64c8346e896f1edfe940bd2c83a5
Blez_Ten <83898491+blezDev@users.noreply.github.com>
2023-03-14 20:58:31 +0530

    sample ui for compiler

---
+++ app/src/main/java/com/blez/compilerx/ui/compiler/CompilerActivity.kt
@@ -0,0 +1,12 @@
+package com.blez.compilerx.ui.compiler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blez.compilerx.R
+
+class CompilerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compiler)
+    }
+}
