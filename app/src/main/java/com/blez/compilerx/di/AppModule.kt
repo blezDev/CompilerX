7856532badb01774e8455add5502c03192a9c4f7
Blez_Ten <83898491+blezDev@users.noreply.github.com>
2023-03-14 21:56:31 +0530

    more fix

--- app/src/main/java/com/blez/compilerx/di/AppModule.kt
+++ app/src/main/java/com/blez/compilerx/di/AppModule.kt
@@ -7,13 +7,29 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() : OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .writeTimeout(1, TimeUnit.MINUTES) // write timeout
+            .readTimeout(1, TimeUnit.MINUTES) // read timeout
+            .build()
+
+    }
 
     @Provides
     @Singleton
