34b1b171d73e4709ea2d1fc80c25bb146d8bf4ec
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-03-14 21:53:59 +0530

    UI Done. loader page

--- app/src/main/java/com/blez/compilerx/MainActivity.kt
+++ app/src/main/java/com/blez/compilerx/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.blez.compilerx
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blez.compilerx.ui.compiler.CompilerActivity
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Timer
+import kotlin.concurrent.timerTask
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        changeIntent()
+    }
+    private fun changeIntent(){
+        val timeTasks = timerTask {
+            val intent = Intent(this@MainActivity, CompilerActivity::class.java)
+            startActivity(intent)
+        }
+        val timer = Timer()
+        timer.schedule(timeTasks,5000)
     }
 }
--- app/src/main/java/com/blez/compilerx/di/AppModule.kt
+++ app/src/main/java/com/blez/compilerx/di/AppModule.kt
@@ -6,11 +6,26 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesCompilerAPI(okHttpClient: OkHttpClient) : CompilerAPI{
+        return Retrofit.Builder()
+            .baseUrl("")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(CompilerAPI::class.java)
+    }
+
 
     @Provides
     @Singleton
